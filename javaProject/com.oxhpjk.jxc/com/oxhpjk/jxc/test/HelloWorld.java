770eb88ca5a06e5853d90b36c340db084d73e3a2
Chunibyou <2434937467@qq.com>
2020-05-17 23:43:43 +0800

    咩都没的原始版本

---
+++ javaProject/com.oxhpjk.jxc/com/oxhpjk/jxc/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.oxhpjk.jxc.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("¸øÒ¯ÅÀ");
+	}
+
+}
